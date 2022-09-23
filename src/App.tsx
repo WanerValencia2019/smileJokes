6525a28efbfc179af2a89f096a7086aace8c1ae0
WanerValencia <waner.valencia@antpack.co>
2022-09-23 17:13:02 -0500

    remove sass file

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
 import AuthProvider from './context/auth/auth.provider'
 import AppRoutes from './routes'
 
-import './styles/styles.scss';
-
-
-
 function App() {
   return (
     <div className="App">
